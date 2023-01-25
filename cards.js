d85d7385e37203df3744218a470ea02e4e746b84
cjegeonu <96885235+cjegeonu@users.noreply.github.com>
2023-01-25 14:43:38 -0600

    finally fixed bugs

---
+++ cards.js
@@ -0,0 +1,51 @@
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+// Next button handler
+const handleNextClick = () => {
+    const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "after";
+
+    nextGroup.dataset.status = "becoming-active-from-before";
+
+    setTimeout(() => {
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
+// Back button handler
+// const handleBackClick = () => {
+//     const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+//     const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+//         nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+//     currentGroup.dataset.status = "before";
+
+//     nextGroup.dataset.status = "becoming-active-from-after";
+
+//     setTimeout(() => {
+//         nextGroup.dataset.status = "active";
+//         activeIndex = nextIndex;
+//     });
+// }
+
+const handleBackClick = () => {
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+         nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    currentGroup.dataset.status = "before";
+
+    nextGroup.dataset.status = "becoming-active-from-after";
+
+    setTimeout(() => {
+        nextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+      });
+    }
